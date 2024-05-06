dd572cec2338622d231c516c38f1e91889d9a658
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-06 20:04:07 +0200

    Add files via upload

---
+++ accueil/script.js
@@ -0,0 +1,26 @@
+function redirect(){
+    window.location.href = "compte.html";
+}
+function redirectCreation(){
+    window.location.href = "creation.html";
+    
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function lancervideo(){
+    const video = document.getElementById('background-video');
+    const button = document.getElementById('start_button');
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const border = document.getElementById('title_border');
+    border.style.opacity = '0';
+    description.style.opacity = '0';
+    title.style.opacity = '0';
+    button.style.opacity = '0';
+    video.play();
+    await sleep(2800);
+    window.location.href = "creation.html";
+}
