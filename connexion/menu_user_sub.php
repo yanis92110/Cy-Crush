56f026e2260134fc12a153a429a9e8d36938be67
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-09 15:22:59 +0200

    Update menu_user_sub.php

--- connexion/menu_user_sub.php
+++ connexion/menu_user_sub.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Accueil sub</title>
-    <link rel="stylesheet" href="styleaccueil.css">
+    <link rel="stylesheet" href="stylemanu.css">
 </head>
 <body>
 
