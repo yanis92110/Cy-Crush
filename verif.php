789986b808617da33b6254efdf4f986e1b9081ed
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-09 16:58:47 +0200

    Add files via upload

---
+++ verif.php
@@ -0,0 +1,54 @@
+<?php
+function list_data($chemin, $k){
+    if(is_int($k) && ($k <= 9)){
+        $list = array();
+
+        if(($file = fopen("$chemin", "r")) !== FALSE){
+            while(($data = fgetcsv($file, 1000, ",")) !== FALSE){
+                $list[] = $data[$k];
+            }
+        fclose($file);
+        }
+        return $list;
+    }
+}
+
+/*foreach ($lists as $list)
+    echo $list , "<br>";
+*/
+
+function get_data1($k){
+    $chemin = '../data/data1.csv';
+    $data1 = fopen($chemin,"r");
+    $num_ligne = 1;
+    $data = array();
+
+    while(($ligne = fgetcsv($data1)) !== FALSE){
+        if($num_ligne === $k){
+            $data = $ligne;
+            break;
+        }
+        $num_ligne++;
+    }
+    fclose($data1);
+    return $data;
+}
+
+function get_data2($k){
+    $chemin = '../data/data2.csv';
+    $data2 = fopen($chemin,"r");
+    $num_ligne = 1;
+    $data = array();
+
+    while(($ligne = fgetcsv($data2)) !== FALSE){
+        if($num_ligne === $k){
+            $data = $ligne;
+            break;
+        }
+        $num_ligne++;
+    }
+    fclose($data2);
+    return $data;
+}
+
+?>
