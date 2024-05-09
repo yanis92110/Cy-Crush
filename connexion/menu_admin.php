903f611008ab79edca207ea5a280c3f61adca96f
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-09 15:18:49 +0200

    Update menu_admin.php

--- connexion/menu_admin.php
+++ connexion/menu_admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Accueil admin</title>
-    <link rel="stylesheet" href="styleaccueil.css">
+    <link rel="stylesheet" href="stylemenu.css">
 </head>
 <body>
 
