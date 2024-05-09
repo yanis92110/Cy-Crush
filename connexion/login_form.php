4e95d17ea3ef6634de7aab4d5b02ac55e592bf96
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-09 13:42:25 +0200

    Add files via upload

---
+++ connexion/login_form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="styleconnexion.css">
+</head>
+<body>
+  <h1>Formulaire de connexion</h1>
+
+  <div class="form-container">
+    <form id="loginForm">
+      <h2>Connexion</h2>
+      <div>
+        <label for="loginPseudo" method="post">Pseudo</label>
+        <input type="text" id="loginPseudo" required>
+      </div>
+      <div>
+        <label for="loginMdp">Mot de Passe</label>
+        <input type="password" id="loginMdp" required>
+      </div>
+      <button type="submit">Se Connecter</button>
+    </form>
+    <img id="court" src="tennis_court.jpg">
+    </div>
+</body>
+</html>
