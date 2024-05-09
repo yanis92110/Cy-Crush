89efa4c77475615f68e11db97e8edd460514bf15
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-09 15:21:29 +0200

    Update login.php

--- connexion/login.php
+++ connexion/login.php
@@ -20,16 +20,16 @@ if(isset($_POST['pseudo_login']) && isset($_POST['mdp_login'])){
             $_SESSION['pseudo'] = $pseudo_login;
             $_SESSION['mdp'] = $mdp_login;
             if($list_admin[$indice] === 1){
-                header("Location: accueil_admin.php");
+                header("Location: menu_admin.php");
                 exit();
             }
             else{
                 if($list_sub[$indice] === 1){
-                    header("Location: accueil_user_sub.php");
+                    header("Location: menu_user_sub.php");
                     exit();
                 }
                 else{
-                    header("Location: accueil_user_nonsub.php");
+                    header("Location: menu_user_nonsub.php");
                     exit();
                 }
             }
