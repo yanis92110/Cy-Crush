970ae20fd61d189865e914cf03e8bc8fb1082e48
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-13 19:01:06 +0200

    Update script_upload.js

--- menu/script_upload.js
+++ menu/script_upload.js
@@ -9,3 +9,7 @@ function redirect_profile(){
 function redirect_pfp(){
     window.location.href = "pfp.php";
 }
+
+function redirect_modif_infos(){
+    window.location.href = "modif_info_form.php";
+}
