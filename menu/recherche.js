02fdc803cbef624cefca5e138cb7991c0d706e19
yanis92110 <126026727+yanis92110@users.noreply.github.com>
2024-05-19 14:40:30 +0200

    Update recherche.js

--- menu/recherche.js
+++ menu/recherche.js
@@ -7,7 +7,11 @@ function recherche(){
     
     var liste=[];
     liste.push(sexe, classement, mano, lrevers, autre);
-
-    alert(liste);
+    for(var i=0; i<liste.length;i=i+1){
+        //A mon avis, trier dans l'ordre les data pour faciliter la recherche
+        if(liste[i]!=""){
+            
+        }
+    }
 }
 recherche();
