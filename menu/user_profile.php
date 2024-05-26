81ca637e57f0f2aefca1fb6351cb95d6c10d6f17
JeanAnd <125206031+JeanAnd@users.noreply.github.com>
2024-05-26 23:23:10 +0200

    Update user_profile.php

--- menu/user_profile.php
+++ menu/user_profile.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Profil</title>
+    <link rel="stylesheet" type="text/css" href="profil.css">
+    <link rel="stylesheet" type="text/css" href="menu.css">
+    <link rel="stylesheet" type="text/css" href="/messagerie/base/style.css">
+</head>
+<body>
+
 <?php
 session_start();
 include "../verif.php";
@@ -9,39 +20,15 @@ $data2 = get_data2($indice);
 $data2 = array_slice($data2,1,null,true);
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Profil</title>
-    <link rel="stylesheet" type="text/css" href="profil.css">
-    <link rel="stylesheet" type="text/css" href="menu.css">
-    <link rel="stylesheet" type="text/css" href="/messagerie/base/style.css">
-</head>
-<body>
-    <h2 id="logo"> CY Roland </h2>
-    <h2 id="logobis"> Garros </h2>
 
     <div class="global">
-
             <div class="button-container">
-                <?php
-                $tempdata1=get_data1($_SESSION['id_user']);
-                if($tempdata1[8]==1){
-                    $redirect="redirect_admin()";
-                }
-                else if($tempdata1[4]==0){
-                    $redirect="redirect_non_sub()";
-                }
-                else if($tempdata1[4]== 1){
-                    $redirect= "redirect_sub()";
-                }
-                echo '<button class="button2" onclick='.$redirect.'>';
-                echo '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024" stroke-width="0" fill="currentColor" stroke="currentColor" class="icon">';
-                    echo '<path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>';
-                echo "</svg>";
-                echo "</button>";
-                ?>
+                <button class="button" onclick="redirect_home()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024" stroke-width="0" fill="currentColor" stroke="currentColor" class="icon">
+                    <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>
+                </svg>
+                </button>
+
                 <button class="button" onclick="redirect_search()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
                     <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke-linejoin="round" stroke-linecap="round"></path>
@@ -62,22 +49,18 @@ $data2 = array_slice($data2,1,null,true);
                 </svg>
                 </button>
 
-                <button class="button" id="logoutBtn" onclick="redirect_accueil()">
-                    <svg viewBox="0 0 512 512" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="0" fill="currentColor" stroke="currentColor" class="icon">
-                        <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
-                    </svg>
-                </button>
-
             </div>
     
         <div class="container">
             <?php
-            $dir = "../img/".$data1[0]."/".$data1[0].".jpg";
-            if(isset($dir))
-                echo "<img class='pfp' src='{$dir}' alt='Photo de profil'>";
+            if(isset($_SESSION['pfp'.$_SESSION['pseudo']]))
+                echo "<img class='pfp' src='{$_SESSION['pfp'.$_SESSION['pseudo']]}' alt='Photo de profil'>";
+            else if (isset($_COOKIE['pfp'.$_SESSION['pseudo']])){
+                $_SESSION['pfp'.$_SESSION['pseudo']] = $_COOKIE['pfp'.$_SESSION['pseudo']];
+                echo "<img class='pfp' src='{$_SESSION['pfp'.$_SESSION['pseudo']]}' alt='Photo de profil'>";
+            }
             else{
-                
-                echo "<img class='pfp' src='../default.jpg' alt='Photo de profil'>";
+                echo "<img class='pfp' src='../img/default.png' alt='Photo de profil'>";
             }
             ?>
             <br>
