6b4aeebe3f682445741cb22158408db6d2bffe53
yanis92110 <126026727+yanis92110@users.noreply.github.com>
2024-05-26 23:22:58 +0200

    Update script_upload.js

--- menu/script_upload.js
+++ menu/script_upload.js
@@ -2,8 +2,8 @@ function redirect_upload(){
     window.location.href="upload.php";
 }
 
-function redirect_profile(){
-    window.location.href = "user_profile.php";
+function redirect_profil(){
+    window.location.href = "../menu/user_profil.php";
 }
 
 function redirect_pfp(){
@@ -13,31 +13,38 @@ function redirect_pfp(){
 function redirect_modif_infos(){
     window.location.href = "modif_info_form.php";
 }
-
-function redirect_home(){
-    window.location.href = "menu_user_sub.php";
+function redirect_sub(){
+    window.location.href="../menu/menu_user_sub.php";
 }
-
+function redirect_non_sub(){
+    window.location.href="../menu/menu_user_nonsub.php";
+}
+function redirect_admin(){
+    windpw.location.href="../menu/menu_admin.php";
+}
 function redirect_search(){
-    window.location.href = "recherche_form.html";
+    window.location.href="../menu/recherche_form.php";
 }
-
-function redirect_profil(){
-    window.location.href = "user_profile.php";
-}
-
 function redirect_store(){
-    document.location.href = "abonnements.php";
+    window.location.href="../accueil/abonnements.php";
 }
-
-function redirect_accueil(){
-    document.location.href = "../accueil/accueil.html";
-}
-
-document.addEventListener("load",function(){
-    afficher10DerniersInscrits();
-});
-
-function redirect_Sub(){
+function redirect_paiement(){
+    //renvoie vers le paiement
     window.location.href="../menu/paiement.php";
 }
+
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    fetch("destroy_session.php", {
+        method: "POST"
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
+            console.log(data.message); // "Session détruite avec succès."
+            window.location.href ="../accueil/accueil.html";
+        } else {
+            console.error("Erreur lors de la destruction de la session.");
+        }
+    })
+    .catch(error => console.error("Erreur:", error));
+});
